b47600d21eefc3c877e675449284c431cd970941
WonderBoy7 <nyanjamitiarazafimahefa@gmail.com>
2024-01-27 11:30:56 +0300

    create class

---
+++ src/main/java/com/ced/app/repository/VenteRepository.java
@@ -0,0 +1,4 @@
+package com.ced.app.repository;
+
+public interface VenteRepository {
+}
---
+++ src/main/java/com/ced/app/service/VenteService.java
@@ -0,0 +1,8 @@
+package com.ced.app.service;
+
+import com.ced.app.model.Utilisateur;
+
+public class VenteService {
+    public void Vendre(int idVender , int idConsommateur,double prixdevente,int idVoiture){
+    }
+}
