26d493a5408d5e73ad90f86ccbb699a93a833b91
Tiavina-Itokiana <tiavinaitokiana@gmail.com>
2024-01-28 21:57:50 +0300

    set up marque et categorie
    nalana tao anaty vaovao ilay categoie sy marque de nampandehanina ilay projet, manova anarana base sy user ary mdp ao anaty application.properties de mandeha io

--- src/main/java/com/ced/app/repository/MarqueRepository.java
+++ src/main/java/com/ced/app/repository/MarqueRepository.java
@@ -4,8 +4,12 @@ package com.ced.app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ced.app.model.Marque;
+import org.springframework.data.jpa.repository.Query;
+
 import java.lang.Integer;
+import java.util.Optional;
 
 public interface MarqueRepository extends JpaRepository<Marque, Integer> {
-    
+    @Query("SELECT c FROM Marque c WHERE c.nom=?1 ")
+    Optional<Marque> findByNom(String prenom);
 }
